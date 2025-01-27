7651b8d95c90ab4df58a39e403231bd29852bd8c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-01-27 14:11:31 +0100

    add test with ktest/kwok
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- src/controller.rs
+++ src/controller.rs
@@ -30,3 +30,66 @@ pub struct PolicyRevisionSpec {
 
 #[derive(Deserialize, Serialize, Clone, Default, Debug, JsonSchema)]
 pub struct PolicyRevisionStatus {}
+
+#[cfg(test)]
+mod test {
+    use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
+    use ktest::ktest;
+    use kube::{
+        api::ObjectMeta,
+        runtime::{conditions, wait::await_condition},
+        CustomResourceExt,
+    };
+
+    use super::*;
+
+    #[ktest]
+    #[tokio::test]
+    async fn test_create_crd() {
+        // TODO: should this be moved to ktest?
+        let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
+        let ssapply = PatchParams::apply("test").force();
+        crds.patch(
+            "policyrevisions.policies.kubewarden.io",
+            &ssapply,
+            &Patch::Apply(PolicyRevision::crd()),
+        )
+        .await
+        .unwrap();
+
+        let establish = await_condition(
+            crds,
+            "policyrevisions.policies.kubewarden.io",
+            conditions::is_crd_established(),
+        );
+        let _ = tokio::time::timeout(std::time::Duration::from_secs(10), establish)
+            .await
+            .unwrap();
+
+        let api: Api<PolicyRevision> = Api::default_namespaced(client);
+
+        let pp = kube::api::PostParams::default();
+        let _ = api
+            .create(
+                &pp,
+                &PolicyRevision {
+                    metadata: ObjectMeta {
+                        name: Some("test-policy-revision".to_string()),
+                        namespace: Some("default".to_string()),
+                        ..Default::default()
+                    },
+                    spec: PolicyRevisionSpec {
+                        data: RawExtension(serde_json::json!({
+                            "name": "test-policy",
+                            "version": "v1",
+                            "rules": [],
+                        })),
+                        enabled: true,
+                    },
+                    status: Default::default(),
+                },
+            )
+            .await
+            .expect("Failed to create PolicyRevision");
+    }
+}
